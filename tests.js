02be53297a94c3b42f1be449cd1b6a67f89ad7e8
Minh Nguyen <quangminh.nguyen8989@gmail.com>
2023-01-25 16:59:30 -0600

    commit with sayHello test function

--- tests.js
+++ tests.js
@@ -21,10 +21,10 @@ describe('sayHello', function() {
     it('should return a string when called', function() {
         expect(typeof sayHello()).toBe("string");
     });
-    it('should return the string "Say, Hello!" when executed', function() {
-        expect(sayHello()).toBe("Say, Hello!");
+    it("Should return a string \"Say, Hello\" when executed", function () {
+        expect(sayHello()).toBe("Say, Hello");
     });
-    it("should never return 'undefined' when called", function() {
+    it("should never return \"undefined\" when called", function () {
         expect(sayHello()).not.toBe(undefined);
     });
 });
